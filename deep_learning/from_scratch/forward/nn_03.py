ed5c3a5a61f432eb30a91d4fffacf38482a56fdb
Morteza <maleki.morteza92@gmail.com>
2025-03-10 09:49:25 +0330

    added from scratch

---
+++ deep_learning/from_scratch/forward/nn_03.py
@@ -0,0 +1,92 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+class LayerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class ActivationReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class ActivationSoftmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                            keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1,
+                                            keepdims=True)
+        self.output = probabilities
+
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class LossCategoricalCrossentropy(Loss):
+
+    def forward(self, y_pred, y_true):
+        # Number of samples in a batch
+        samples = len(y_pred)
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+            range(samples),
+            y_true
+            ]
+        # Mask values - only for one-hot encoded labels
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+            y_pred_clipped * y_true,
+            axis=1
+            )
+        # Losses
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = LayerDense(2, 3)
+activation1 = ActivationReLU()
+
+dense2 = LayerDense(3, 3)
+
+activation2 = ActivationSoftmax()
+
+loss_function = LossCategoricalCrossentropy()
+
+
+dense1.forward(X)
+
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+
+
+activation2.forward(dense2.output)
+
+loss = loss_function.calculate(activation2.output, y)
+print(activation2.output[:5])
+
+print('loss:', loss)
+# Calculate accuracy from output of activation2 and targets
+# calculate values along first axis
+predictions = np.argmax(activation2.output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis=1)
+accuracy = np.mean(predictions==y)
+# Print accuracy
+print('acc:', accuracy)
---
+++ deep_learning/from_scratch/forward/nn_04.py
@@ -0,0 +1,101 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import vertical_data
+
+nnfs.init()
+
+class LayerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class ActivationReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class ActivationSoftmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                            keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1,
+                                            keepdims=True)
+        self.output = probabilities
+
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class LossCategoricalCrossentropy(Loss):
+
+    def forward(self, y_pred, y_true):
+        # Number of samples in a batch
+        samples = len(y_pred)
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[
+            range(samples),
+            y_true
+            ]
+        # Mask values - only for one-hot encoded labels
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+            y_pred_clipped * y_true,
+            axis=1
+            )
+        # Losses
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
+# Create dataset
+X, y = vertical_data(samples=100, classes=3)
+# Create model
+dense1 = LayerDense(2, 3) # first dense layer, 2 inputs
+activation1 = ActivationReLU()
+dense2 = LayerDense(3, 3) # second dense layer, 3 inputs, 3 outputs
+activation2 = ActivationSoftmax()
+# Create loss function
+loss_function = LossCategoricalCrossentropy()
+# Helper variables
+lowest_loss = 9999999 # some initial value
+best_dense1_weights = dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense2.weights.copy()
+best_dense2_biases = dense2.biases.copy()
+for iteration in range(10000):
+    # Generate a new set of weights for iteration
+    dense1.weights = 0.05 * np.random.randn(2, 3)
+    dense1.biases = 0.05 * np.random.randn(1, 3)
+    dense2.weights = 0.05 * np.random.randn(3, 3)
+    dense2.biases = 0.05 * np.random.randn(1, 3)
+    # Perform a forward pass of the training data through this layer
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+    # Perform a forward pass through activation function
+    # it takes the output of second dense layer here and returns loss
+    loss = loss_function.calculate(activation2.output, y)
+    # Calculate accuracy from output of activation2 and targets
+    # calculate values along first axis
+    predictions = np.argmax(activation2.output, axis=1)
+    accuracy = np.mean(predictions==y)
+    # If loss is smaller - print and save weights and biases aside
+    if loss < lowest_loss:
+        print('New set of weights found, iteration:', iteration,
+        'loss:', loss, 'acc:', accuracy)
+        best_dense1_weights = dense1.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense2_biases = dense2.biases.copy()
+        lowest_loss = loss
